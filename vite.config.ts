bb5f691e1e63c0dd92eebfe335fbd934ae675208
egiistw88 <egiegay08@gmail.com>
2025-12-23 02:30:11 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
     const env = loadEnv(mode, process.cwd(), '');
-    
-    // Ambil API Key dari process.env (Vercel System Env) ATAU file .env
     const apiKey = process.env.GEMINI_API_KEY || env.GEMINI_API_KEY;
 
     return {
@@ -17,8 +13,6 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        // Stringify kunci agar tertanam sebagai string di kode frontend
-        // Jika kosong, kita kasih string kosong biar gak crash 'undefined'
         'process.env.API_KEY': JSON.stringify(apiKey || ''),
         'process.env.GEMINI_API_KEY': JSON.stringify(apiKey || '')
       },
